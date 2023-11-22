09018e1765c7a474b768f66ffcd0179273019a23
KotohaSaki <kotoha.saki.14@gmail.com>
2023-11-22 20:10:28 +0900

    storelist,service

---
+++ JS/storelist.js
@@ -0,0 +1,198 @@
+JavaScript
+function viewChange(){
+    if(document.getElementById('storelist')){
+        id = document.getElementById('storelist').value;
+        if(id == 'select0'){
+            document.getElementById('Box0').style.display = "";
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }else if(id == 'select1'){
+            document.getElementById('Box0').style.display = "none";
+            document.getElementById('Box1').style.display = "";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }else if(id == 'select2'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+        else if(id == 'select3'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+        else if(id == 'select4'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select5'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select6'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select7'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select8'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select9'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select10'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select11'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "";
+            document.getElementById('Box12').style.display = "none";
+        }
+
+        else if(id == 'select12'){
+            document.getElementById('Box1').style.display = "none";
+            document.getElementById('Box2').style.display = "none";
+            document.getElementById('Box3').style.display = "none";
+            document.getElementById('Box4').style.display = "none";
+            document.getElementById('Box5').style.display = "none";
+            document.getElementById('Box6').style.display = "none";
+            document.getElementById('Box7').style.display = "none";
+            document.getElementById('Box8').style.display = "none";
+            document.getElementById('Box9').style.display = "none";
+            document.getElementById('Box10').style.display = "none";
+            document.getElementById('Box11').style.display = "none";
+            document.getElementById('Box12').style.display = "";
+        }
+    }
+
+window.onload = viewChange;
+}
